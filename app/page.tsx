db83eedbc0f9f7571a8fa44b73924bb8f7cbaaa5
thesanimoney <thesanimoney@gmail.com>
2024-08-20 16:14:30 +0300

    first release

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,10 @@ import { Bot, CornerDownLeft, Smile, Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Instruction from "@/components/instruction";
 import { useRef, useEffect } from 'react';
+import {questions} from "@/components/questions";
 
 export default function Chat() {
-    const { messages, input, handleInputChange, handleSubmit, setMessages } = useChat({
+    const { messages, input, handleInputChange, handleSubmit, setMessages, setInput } = useChat({
         maxToolRoundtrips: 2,
         onResponse: () => {
             // Ensure the latest message is visible
@@ -40,7 +41,7 @@ export default function Chat() {
                     className={'text-zinc-500 absolute top-5 right-10 hover:text-red-400'}
                     variant={'ghost'}
                 >
-                    Reset conversation<Trash className={'ml-2'} />
+                    Reset conversation<Trash className={'ml-2'}/>
                 </Button>
             )}
             <div className="space-y-4">
@@ -49,21 +50,22 @@ export default function Chat() {
                         <div key={m.id} className="whitespace-pre-wrap">
                             <div>
                                 <h4 className="font-bold mb-1 flex gap-x-1 items-center">
-                                     {m.role === 'assistant' ? (
+                                    {m.role === 'assistant' ? (
                                         <div className={'rounded-full bg-blue-100 p-1'}>
-                                            <Bot />
+                                            <Bot/>
                                         </div>
                                     ) : (
                                         <div className={'rounded-full bg-zinc-100 p-1'}>
-                                            <Smile />
+                                            <Smile/>
                                         </div>
                                     )}
                                     {m.role[0].toUpperCase() + m.role.slice(1)}
                                 </h4>
-                                <div className={m.role === 'assistant' ? `text-sm text-gray-600 bg-blue-50 p-2 rounded-md` :
-                                    'text-sm text-gray-600 bg-zinc-50 p-2 rounded-md'}>
+                                <div
+                                    className={m.role === 'assistant' ? `text-sm text-gray-600 bg-blue-50 p-2 rounded-md` :
+                                        'text-sm text-gray-600 bg-zinc-50 p-2 rounded-md'}>
                                     {m.content.length > 0 ? (
-                                        <DynamicTextRenderer text={m.content} />
+                                        <DynamicTextRenderer text={m.content}/>
                                     ) : (
                                         <span className="italic font-light">
                                             {'calling tool: ' + m?.toolInvocations?.[0]?.toolName}
@@ -74,13 +76,20 @@ export default function Chat() {
                         </div>
                     ))
                 ) : (
-                    <Instruction />
+                    <Instruction/>
                 )}
-                <div ref={endOfMessagesRef} />
+                <div ref={endOfMessagesRef}/>
             </div>
+
             <form onSubmit={handleSubmit}>
                 <div
                     className="fixed text-zinc-500 text-sm bottom-0 w-full max-w-[768px] p-3 mb-8 border border-gray-300 rounded shadow-xl">
+                    {messages.length <= 0 &&
+                        <div className="text-xs w-full absolute flex justify-between -top-14 left-0">
+                            {questions.map(({question}, index) =>
+                                <Button className={'opacity-60 font-normal'} key={index} variant={'secondary'}
+                                        onClick={() => setInput(question)}>{question}</Button>)}
+                        </div>}
                     <input
                         className="w-[90%] p-1 rounded-md outline-none bg-transparent"
                         value={input}
@@ -88,7 +97,7 @@ export default function Chat() {
                         onChange={handleInputChange}
                     />
                     <Button className="absolute right-2 top-2 text-zinc-500" size="sm" variant="ghost">
-                        <CornerDownLeft />
+                        <CornerDownLeft/>
                     </Button>
                 </div>
             </form>
---
+++ components/questions.tsx
@@ -0,0 +1,6 @@
+export const questions = [
+    {question: "What is WCG's core mission and vision?"},
+    {question: "What are the primary products or services offered by WCG?"},
+    // {question: "What technologies or tools does WCG use to streamline its operations?"},
+    // {question: "What are the main operational processes at WCG"},
+]
